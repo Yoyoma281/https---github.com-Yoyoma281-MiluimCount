30af74056e58fe8b902ba8e182d22666773b5cef
yoyoma281 <shai239@gmail.com>
2025-04-24 17:09:27 +0300

    removed dotenv requirement.

--- TrackerServer/api/index.js
+++ TrackerServer/api/index.js
@@ -2,7 +2,6 @@ const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
 const serverless = require("serverless-http");
-require("dotenv").config();
 
 const app = express();
 
